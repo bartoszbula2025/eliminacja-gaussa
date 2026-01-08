82e64f6381dc0c7547626e344e0beacca54a4a73
bartoszbula2025 <01206751@pw.edu.pl>
2026-01-08 17:55:57 +0100

    dodanie zamiany wierszy

--- gauss.c
+++ gauss.c
@@ -26,6 +26,21 @@ void gauss_elimination(int n, double a[n][n], double b[n])
             printf("Blad: macierz osobliwa (kolumna %d)\n", i);
             exit(1);
         }
+        
+        // zamiana wierszy
+        if (max_row != i)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                double temp = a[i][k];
+                a[i][k] = a[max_row][k];
+                a[max_row][k] = temp;
+            }
+
+            double tempb = b[i];
+            b[i] = b[max_row];
+            b[max_row] = tempb;
+        }
 
         // eliminacja
         for (int j = i + 1; j < n; j++)
