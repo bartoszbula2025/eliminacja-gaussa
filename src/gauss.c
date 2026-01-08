90e865ddcc95943979f1f97bc5e8a91afb2d5940
bartoszbula2025 <01206751@pw.edu.pl>
2026-01-08 22:02:07 +0100

    zamiana tablicy na matrix

--- src/gauss.c
+++ src/gauss.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "mat_io.h"
 
-void gauss_elimination(int n, double a[n][n], double b[n])
+void gauss_elimination(Matrix *A, double *b)
 {
-    double factor;
-    
+    if (A->r != A->c)
+    {
+        printf("Blad: macierz nie jest kwadratowa!\n");
+        exit(1);
+    }
+
+    int n = A->r;
+
     for (int i = 0; i < n - 1; i++)
     {
-        // wybor glownego elementu
+        // wybor elementu glownego
         int max_row = i;
-        double max = fabs(a[i][i]);
+        double max = fabs(A->data[i][i]);
 
         for (int r = i + 1; r < n; r++)
         {
-            if (fabs(a[r][i]) > max)
+            if (fabs(A->data[r][i]) > max)
             {
-                max = fabs(a[r][i]);
+                max = fabs(A->data[r][i]);
                 max_row = r;
             }
         }
@@ -26,32 +33,29 @@ void gauss_elimination(int n, double a[n][n], double b[n])
             printf("Blad: macierz osobliwa (kolumna %d)\n", i);
             exit(1);
         }
-        
+
         // zamiana wierszy
         if (max_row != i)
         {
-            for (int k = 0; k < n; k++)
-            {
-                double temp = a[i][k];
-                a[i][k] = a[max_row][k];
-                a[max_row][k] = temp;
-            }
+            double *tmp = A->data[i];
+            A->data[i] = A->data[max_row];
+            A->data[max_row] = tmp;
 
-            double tempb = b[i];
+            double tmpb = b[i];
             b[i] = b[max_row];
-            b[max_row] = tempb;
+            b[max_row] = tmpb;
         }
 
         // eliminacja
         for (int j = i + 1; j < n; j++)
         {
-            factor = a[j][i] / a[i][i];
-            
+            double factor = A->data[j][i] / A->data[i][i];
+
             for (int k = i; k < n; k++)
             {
-                a[j][k] -= factor * a[i][k];
+                A->data[j][k] -= factor * A->data[i][k];
             }
-            
+
             b[j] -= factor * b[i];
         }
     }
